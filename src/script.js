e1b0b8fa9e9821c9c501ab214432f6c6d7fb738d
Maganga Mwambonu <codebymaganga@gmail.com>
2024-09-14 13:02:43 +0300

    Saturday 14thSept

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
+
+let categoryToggle = false;
+let countryToggle = false
+let deliveryToggle = false
+let cartNumber = 1
+
+
 const shopbtn = document.getElementById('shopnow');
 shopbtn.addEventListener('click', function(){
      alert('shop now')
@@ -9,10 +16,95 @@ seebtn.addEventListener('click', function(){
 });
 
 
+//ADD TO CART
+const badgeNo = document.getElementById('badge')
+
+
 const addcartbtn = document.querySelectorAll('#addcart');
 addcartbtn.forEach(function(addcartbtn){
     addcartbtn.addEventListener('click', function(){
-        alert('add cart')
+
+        badgeNo.innerHTML = cartNumber++ 
     });
 });
+
+
+//ADD TO PRICING
+const price = document.getElementById('pricing') 
+const totalPrice = document.getElementById('totalPrice')
+addcartbtn.forEach(function(addcartbtn){
+    addcartbtn.addEventListener('click', function(){
+    //     console.log('text-->',price.innerHTML.split('$'))
+        const result = parseFloat(price.innerHTML.split('$')[1])
+       const totalPriceNum = parseFloat(totalPrice.innerHTML)
+
+       return totalPrice.innerHTML = result + totalPriceNum
+
+
+    
+    });
+});
+
+
+//TOGGLE FOR CATEGORIES
+const categoryButton = document.getElementById('openCategory')
+const categoryList = document.getElementById('categoryList')
+
+    categoryButton.addEventListener('click', ()=>{
+        if(!categoryToggle){
+            categoryList.setAttribute('class', 'block')
+          
+            categoryToggle = true
+        
+        }
+        else{
+            categoryList.setAttribute('class', 'hidden')
+         
+            categoryToggle = false
+        
+        }   
+
+    })
+
+//TOGGLE FOR COUNTRIES
+const countryButton = document.getElementById('countries')
+const countryList = document.getElementById('countryList')
+
+countryButton.addEventListener('click', ()=>{
+        if(!countryToggle){
+            countryList.setAttribute('class', 'block')
+          
+            countryToggle = true
+        
+        }
+        else{
+            countryList.setAttribute('class', 'hidden')
+         
+            countryToggle = false
+        
+        }   
+
+    })
+
+//TOGGLE FOR DELIVERIES
+const delivery = document.getElementById('delivery')
+const deliveryOptions = document.getElementById('deliveryOptions')
+
+delivery.addEventListener('click', ()=>{
+        if(!deliveryToggle){
+            deliveryOptions.setAttribute('class', 'block')
+          
+            deliveryToggle = true
+        
+        }
+        else{
+            deliveryOptions.setAttribute('class', 'hidden')
+         
+            deliveryToggle  = false
+        
+        }   
+
+    })
+    
+
 
